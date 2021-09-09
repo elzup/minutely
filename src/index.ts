1ed149032720a8fc5aec29c1f3eb9e8d90fa4011
elzup <guild0105@gmail.com>
2021-09-09 16:06:48 +0900

    add hourly

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { mensaCheck } from './mensaCheck'
 
 async function main() {
-  // TODO: time
-  mensaCheck()
+  const now = new Date()
+  const h = now.getHours()
+  const min = now.getMinutes()
+  const hourly = min === 0
+  const hourly3 = hourly && h % 3 == 0
+
+  if (hourly3) {
+    mensaCheck()
+  }
 }
 
 main()
