e1b56b47a08a2bc13a5ce2ce9244648e19e33f29
elzup <guild0105@gmail.com>
2021-11-10 13:57:54 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import { slackNotice } from './slackCli'
 async function main() {
   const now = new Date()
   const hourly3 = '* 0 */3 * * * *'
-  const weekly = '* 0 6 * * SAT *'
 
   if (cronMatch(hourly3, now)) {
     mensaCheck()
   }
 
+  const weekly = '* 0 6 * * SAT *'
   if (cronMatch(weekly, now)) {
     slackNotice('weekley', 'minutely works', ':+1:')
   }
