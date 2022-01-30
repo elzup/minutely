94f664401758edd2ba62463b5d43b39f22413382
elzup <guild0105@gmail.com>
2022-01-30 21:33:57 +0900

    add morse2sound option

--- src/morseClockSignal.ts
+++ src/morseClockSignal.ts
@@ -1,8 +1,11 @@
 import { execSync } from 'child_process'
 import * as morse from 'morse-converter'
 
+const cargo = `$HOME/.cargo/bin/cargo`
+const morse2sound = `$HOME/.ghq/github.com/irevenko/morse2sound`
 const morse2soundCmd = (sig: string) =>
-  `(cd ~/.ghq/github.com/irevenko/morse2sound && cargo run "${sig}")`
+  `(cd ${morse2sound}
+    && ${cargo} run --offline --dot-duration 50 --frequency 200 "${sig}")`
 
 export function morseClockSignal() {
   const sig = morse.encode(`${new Date().getHours()}`)
