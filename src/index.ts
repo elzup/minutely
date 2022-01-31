0efca0e217e58e033c3a56c9cc58b54d380dc3fc
elzup <guild0105@gmail.com>
2022-02-01 00:10:37 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,39 @@ import { cronMatch } from './cronMatch'
 import { mensaCheck } from './mensaCheck'
 import { morseClockSignal } from './morseClockSignal'
 
+//
+// It be effective immediately after saving
+//
+
 const dayTime = `8-21`
 const HOURLY = '* 0 * * * *'
 const HOURLY3 = '* 0 */3 * * *'
 const HOURLYDAY = `* 0 ${dayTime} * * *`
 // NOTE: 秒は "*"、1分に一回呼ばれることは保証されていて何秒のタイミングで呼ばれるかは不明なので
 
+type Task = {
+  name: string
+  cronExp: string
+  func: () => void
+}
+const tasks: Task[] = [
+  { name: 'mensaCheck', cronExp: HOURLY3, func: mensaCheck },
+  { name: 'morseClockSignal', cronExp: HOURLY, func: morseClockSignal },
+  // {
+  //   cronExp: WEEKLY,
+  //   name: 'minutely works',
+  //   func: () => slackNotice('weekley', 'minutely works', ':+1:'),
+  // },
+]
+
 async function main() {
-  //
-  // It be effective immediately after saving
-  //
-  const tasks = []
   const now = new Date()
+  const runTasks = tasks.filter((task) => cronMatch(task.cronExp, now))
 
-  if (cronMatch(HOURLY3, now)) {
-    tasks.push(mensaCheck)
-  }
+  if (runTasks.length === 0) return
 
-  // const weekly = '0 0 6 * * SAT *' // not work
-  // if (cronMatch(weekly, now)) {
-  //   slackNotice('weekley', 'minutely works', ':+1:')
-  // }
-
-  if (cronMatch(HOURLY, now)) {
-    tasks.push(morseClockSignal)
-  }
-  if (tasks.length === 0) return
-  tasks.forEach((task) => {
-    task()
+  runTasks.forEach((task) => {
+    task.func()
   })
 }
 
