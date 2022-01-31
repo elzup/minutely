f536790d5c6b13445f130bf0e52db0ae46490bec
elzup <guild0105@gmail.com>
2022-01-31 23:22:23 +0900

    update and refactor signal

--- src/morseClockSignal.ts
+++ src/morseClockSignal.ts
@@ -6,19 +6,39 @@ const morse2sound = `${cargoBin}/morse2sound`
 const morse2soundCmd = (sig: string) =>
   `${morse2sound} --dot-duration 50 --frequency 200 "${sig}"`
 
-const hourSignal = () => {
-  const h = new Date().getHours()
-  // 0 => 'za',
-  // 1 => 'b',
-  // 2 => 'c',
-  // 23=> 'xy'
-  const hourCharList = ['za', ...`bcdefghijklmnopqrstuvw`.split(''), 'xy']
+// 0 => 'za',
+// 1 => 'b',
+// 2 => 'c',
+// 23=> 'xy'
+const _HOUR_CHAR_LIST0 = ['za', ...`bcdefghijklmnopqrstuvw`.split(''), 'xy']
 
-  return `${hourCharList[h]}${h}`
-}
+// prettier-ignore
+const HOUR_CHAR_LIST = [
+  `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`,
+  `ab 9`,
+  `cd 10`,
+  `ef 11`,
+  `gh 12`,
+  `ij 13`,
+  `kl 14`,
+  `mn 15`,
+  `op 16`,
+  `qr 17`,
+  `st 18`,
+  `uv 19`,
+  `wx 20`,
+  `yz 21`,
+  `22`, `23`,
+]
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+export const hourSignal = (d: number, h: number) =>
+  `${ALPHABET[d % 26]} ${HOUR_CHAR_LIST[h]} ${h}`
+
+const hourSignalNow = (d = new Date()) => hourSignal(d.getDate(), d.getHours())
 
 export function morseClockSignal() {
-  const morseSig = morse.encode(hourSignal())
+  const morseSig = morse.encode(hourSignalNow())
   const cmd = morse2soundCmd(morseSig)
 
   execSync(cmd)
