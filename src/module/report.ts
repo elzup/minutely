84e9f15830830661f0156684bb4a4d3854a14645
elzup <guild0105@gmail.com>
2023-12-27 14:16:24 +0900

    change clock time

--- src/module/report.ts
+++ src/module/report.ts
@@ -3,6 +3,6 @@ import { Db } from '../db'
 export async function report(db: Db) {
   const logs = (await db.get('report').value()) || []
 
-  logs.push({ date: new Date().toISOString() })
-  db.set('report', logs.slice(logs.length - 100)).write()
+  logs.push({ date: new Date().toLocaleString() })
+  db.set('report', logs.slice(logs.length - 1000)).write()
 }
